4bd1103ff96d3aa8813107b66fed09c5e2cf3578
CruidGals <kchiem30@gmail.com>
2025-08-20 16:30:52 -0400

    Fixed process not showing up by adding types/node

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/kyle-portfolio",
+  base: process.env.VITE_BASE_PATH || "/kyle-portfolio",
   plugins: [react()]
 })
