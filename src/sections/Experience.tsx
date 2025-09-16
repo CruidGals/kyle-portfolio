27c5c480d967be0673e838e30451890112644a93
CruidGals <kchiem30@gmail.com>
2025-09-16 16:25:28 -0400

    Update Experience.tsx with new resume block

--- src/sections/Experience.tsx
+++ src/sections/Experience.tsx
@@ -3,16 +3,26 @@ import { Container, Row } from "react-bootstrap"
 import { FadeUpH1, FadeUpUl } from "./components/Constants";
 
 const experiences = [
-  { id: 1, 
+  {
+    id: 1,
+    title: 'HackPSU Tech Team',
+    subtitle: 'Software Engineer / Techincal Organizer | Aug 2025 - Present',
+    points: [
+      "Contributed to full-stack Next.js (React, TypeScript) platform supporting 500+ participants during peak hours.",
+      "Maintained and extended a unified NestJS API, modularizing endpoints for authentication, finance, and event services",
+      "Prioritized mobile-first UI through Progressive Web App design, ensuring reliability under unstable WiFi conditions"
+    ]
+  },
+  { id: 2, 
     title: 'The Pennsylvania State University', 
-    subtitle: 'Research Intern - Robotics & Deep Learning | May 2025 - Present',
+    subtitle: 'Research Intern - Robotics & Deep Learning | May 2025 - Sep 2025',
     points: [
       "Built convolutional and reinforcement learning models to predict humanoid robot stability with computer vision.",
       "Executed 4000+ parallel simulations on Isaac Lab to train a locomotion controller for various humanoid robots.",
       "Collected simulated IMU and ego-centric camera data and trained critic functions for stability score prediction.",
       "Integrated domain randomization to create a robust framework capable of in-person deployment (Sim-To-Real)."
     ]},
-  { id: 2, 
+  { id: 3, 
     title: 'Laboratory for Perception, Action, and Cognition', 
     subtitle: 'Undergraduate Researcher | February 2025 - Present',
     points: [
@@ -20,21 +30,14 @@ const experiences = [
       "Contributed in developing a transformer, achieving SOTA performance in foot pressure prediction and setting a new benchmark of 15.95 mm mean error in center-of-mass estimation with movement video as input.",
       "Optimized neural network through hyperparameter tuning with Optuna, improving prediction performance by 13%"
     ]},
-  { id: 3,
+  { id: 4,
     title: 'Nittany AI Alliance Student Chapter',
     subtitle: 'Machine Learning Apprentice | January 2025 - May 2025',
     points: [
       "Designed a RAG Chatbot using Sentence Transformers and Tiny LLaMa LLM for finance-related Q&A over PDFs.",
       "Preprocessed 100,000+ NIH Chest X-ray data to train a CNN to detect pneumonia patients at an accuracy of 92%.",
       "Explored overfitting control, RL reward shaping, and model evaluation with precision and confusion matrix analysis."
-    ]},
-  { id: 4, 
-    title: 'Temple University', 
-    subtitle: 'Summer Research Assistant | June 2023 - August 2023',
-    points: [
-      "Researched Graph Neural Networks to boost node connection prediction accuracy by 10% under Dr. Hongchang Gao.",
-      "Utilized the PyTorch API on Google Colab to implement and tune optimized neural networks"
-    ]},
+    ]}
 ];
 
 const Experience = () => {
